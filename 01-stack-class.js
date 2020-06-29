c8ded1847575b2289d2d75082aad895f2a3f53ad
Jae Kim <jckim81@yahoo.com>
2020-06-29 18:08:44 -0400

    completed q1

--- 01-stack-class.js
+++ 01-stack-class.js
@@ -1,6 +1,48 @@
 /*
 1. Create a stack class
 Walk through the Stack class in the curriculum and understand it well. Then write a Stack class with its core functions (push, pop) from scratch.
-
-
 */
+
+
+// Creates a node containing the data and a reference to the next item
+class _Node {
+    constructor(data, next) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.top = null
+    }
+
+    push(data) {
+        /* If the stack is empty, then the node will be the
+        top of the stack */
+        if (this.top === null) {
+            this.top = new _Node(data, null);
+            return this.top;
+        }
+
+        /* If the stack already has something, 
+        then create a new node,
+        add data to the new node, and
+        have the pointer point to the top */
+        const node = new _Node(data, this.top);
+        this.top = node;
+    }
+
+    pop() {
+        /* In order to remove the top of the stack, you have to point
+           the pointer to the next item and that next item becomes the
+           top of the stack */
+        const node = this.top;
+        this.top = node.next;
+        return node.data;
+    }
+
+}
+
+
+module.exports = Stack
